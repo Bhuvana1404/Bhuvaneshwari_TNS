a8f92b8b9adba7a424257df5bda8f44f89bf206d
Bhuvana1404 <bb6250033@gmail.com>
2025-08-07 22:02:17 +0530

    Encapsulation

---
+++ src/encapsulation/EncapsulationMain.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class EncapsulationMain {
+
+	public static void main(String[] args) {
+		EncapsulationStudent stu=new EncapsulationStudent();
+		stu.setName("Bhuvana");
+		stu.setAge(20);
+		
+System.out.println(stu);
+	}
+
+}

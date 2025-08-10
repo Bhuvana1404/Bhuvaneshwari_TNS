9c7db75b3812674a511647ddb0da6efc8e1408a4
Bhuvana1404 <bb6250033@gmail.com>
2025-08-10 20:31:17 +0530

    java

---
+++ src/accessmodifier/Testaccess.java
@@ -0,0 +1,18 @@
+package accessmodifier;
+
+public class Testaccess {
+
+	public static void main(String[] args) {
+		
+		AccessModifier ame = new AccessModifier();
+		
+		System.out.println("Public Variable: "+ ame.publicVar);
+		System.out.println("Protected Variable: "+ ame.protectedVar);
+		System.out.println("Default Variable: "+ ame.defaultVar);
+
+	}
+
+}
+
+
+
